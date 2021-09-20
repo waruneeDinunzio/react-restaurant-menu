299ed9b60e2595c9f17edd3fe59aa2ff1af9cc2a
Warunee Dinunzio <dinunziow@gmail.com>
2021-09-20 17:20:55 -0400

    add contact form and add more CSS

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,18 +1,20 @@
 import React from 'react';
-
+import SocialMedia from '../pages/SocialMedia';
 const Footer = () => {
     return (
         <div id="footer">
+            
           <div>
-              <h4>OPENING HOURS</h4>
-              <h5>Lunch:</h5>
+            <h4>OPENING HOURS:</h4>
+              <h5>Lunch</h5>
               <p>Mon-Sat: 11:30 - 3:00 pm</p>
-              <h5>Dinner:</h5>
+              <h5>DINNER</h5>
               <p>Mon-Thurs: 5:00 - 9:00 pm</p>
               <p>Sun: 4:00 - 9:00 pm</p>
           </div>
+          <SocialMedia />
           <div>
-              <h4>DELIVERY HOURS</h4>
+              <h4>DELIVERY HOURS:</h4>
               <p>Mon: Pick Up Only</p>
               <p>Tue-Sat: 11:30 am - 9:00 pm</p>
               <p>Sun: 4:00 - 9:00 pm</p>
