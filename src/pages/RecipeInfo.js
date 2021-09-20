ba5a634398e04ee0cac40f6ab981e82a2a24716d
Warunee Dinunzio <dinunziow@gmail.com>
2021-09-20 05:00:26 -0400

    get error from Testimonials.js. no longer fetch api ??

--- src/pages/RecipeInfo.js
+++ src/pages/RecipeInfo.js
@@ -7,9 +7,12 @@ const RecipeInfo = ({recipes}) => {
           {recipes.map((recipe, index) => {
             
             return (
-              <div>
-                <h2>{recipe.recipe.label}</h2>
+              <div className="recipeInfo">
                 <img src={recipe.recipe.image} alt= "picture"/>
+                <h3>{recipe.recipe.label}</h3>
+                <p>{recipe.recipe.calories}calories</p>
+                <p>{recipe.recipe.ingredients} ingredients</p>
+                <p><a href={recipe.recipe.url}>{recipe.recipe.source}</a></p>
               </div>
             );
           })}
